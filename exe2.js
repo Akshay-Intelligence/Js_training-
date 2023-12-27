4519e15cf46dc91d5e9692acafa8fc3393073f7a
Akshay-Intelligence <akshayvprofile@gmail.com>
2023-12-27 17:51:53 +0530

    added exercise 2

---
+++ exe2.js
@@ -0,0 +1,16 @@
+(function messg(){
+    console.log("Loading User Details...")
+})()
+
+const printGreetings = (()=>{   
+    
+    let txt ="Name:"
+    return innerfunc=(firstName, lastName)=>{
+        txt+= `Greeting_MSG ${firstName} ${lastName}`;
+        return txt
+    }
+ 
+})()
+
+console.log(printGreetings("Akshay" , "viswanathan"))
+
